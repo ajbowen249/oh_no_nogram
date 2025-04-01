afeee59f5522e5c4c55b23ba3e720c5d7a8de491
Alex Bowen <ajbowen249@gmail.com>
2025-04-01 19:46:59 -0400

    feat: show column runs

--- puzzle.lua
+++ puzzle.lua
@@ -1,5 +1,5 @@
 g_puzzle_state = nil
-C_CELL_SIZE = 5
+C_CELL_SIZE = 6
 C_BOARD_SIZE = C_CELL_SIZE * 15
 
 C_PUZ_NULL = 0
@@ -29,27 +29,31 @@ function init_puzzle(index)
   end
 
   for row in all(puzzle.image) do
-    local run_length = 0
-    local row_runs = {}
-    for col in all(row) do
-      if col == 1 then
-        run_length += 1
-      else
-        if run_length > 0 then
-          add(row_runs, run_length)
-          run_length = 0
-        end
-      end
+    add(g_puzzle_state.row_runs, get_list_runs(row))
+  end
+
+  for coli=1, #puzzle.image do
+    local col = {}
+    for rowi=1, #puzzle.image do
+      add(col, puzzle.image[rowi][coli])
     end
 
-    if run_length > 0 or #row_runs == 0 then
-      add(row_runs, run_length)
-    end
-
-    add(g_puzzle_state.row_runs, row_runs)
+    add(g_puzzle_state.col_runs, get_list_runs(col))
   end
 
   g_game_state = C_PUZZLE
+end
+
+function print_run_number(run_val, run_x, y, i)
+  if run_val < 10 then
+    print(run_val, run_x, y, i % 2 == 0 and 4 or 15)
+  else
+    if i % 2 != 0 then
+      pal(4, 15)
+    end
+    spr(run_val - 4, run_x, y)
+    pal()
+  end
 end
 
 function draw_puzzle()
@@ -61,35 +65,36 @@ function draw_puzzle()
 
   rectfill(start_x, start_y, board_end_x, board_end_y, 15)
   rect(start_x, start_y, board_end_x, board_end_y, 4)
+  local cursor_loc = nil
 
   for i, row in ipairs(g_puzzle_state.grid) do
     local y = start_y + ((i - 1) * C_CELL_SIZE)
     rect(start_x, y + C_CELL_SIZE, board_end_x, y + C_CELL_SIZE, 4)
 
-    local row_runs = g_puzzle_state.row_runs[i]
-
     if i == g_puzzle_state.y then
       rectfill(2, y, start_x - 1, y + C_CELL_SIZE - 1, 12)
     end
 
+    local row_runs = g_puzzle_state.row_runs[i]
     for ri=#row_runs, 1, -1 do
       local run_x = start_x - 5 - ((#row_runs - ri) * 5)
-      local run_val = row_runs[ri]
-      if run_val < 10 then
-        print(run_val, run_x, y, i % 2 == 0 and 4 or 15)
-      else
-        if i % 2 != 0 then
-          pal(4, 15)
-        end
-        spr(run_val - 4, run_x, y)
-        pal()
-      end
+      print_run_number(row_runs[ri], run_x, y, i)
     end
 
     for j, cell in ipairs(row) do
       local x = start_x + ((j - 1) * C_CELL_SIZE)
       if i == 1 then
         rect(x + C_CELL_SIZE, start_y, x + C_CELL_SIZE, board_end_y, 4)
+
+        if j == g_puzzle_state.x then
+          rectfill(x, 2, x + C_CELL_SIZE - 1, start_y - 1, 12)
+        end
+
+        local col_runs = g_puzzle_state.col_runs[j]
+        for ci=#col_runs, 1, -1 do
+          local run_y = start_y - 5 - ((#col_runs - ci) * 5)
+          print_run_number(col_runs[ci], x + i, run_y, ci)
+        end
       end
 
       if cell == C_PUZ_MARKED then
@@ -99,9 +104,13 @@ function draw_puzzle()
       end
 
       if i == g_puzzle_state.y and j == g_puzzle_state.x then
-        spr(2, x, y)
+        cursor_loc = { x = x, y = y }
       end
     end
+  end
+
+  if cursor_loc != nil then
+    spr(2, cursor_loc.x, cursor_loc.y)
   end
 end
 
--- utils.lua
+++ utils.lua
@@ -6,3 +6,24 @@ function get_string_pixel_width(string)
 
   return width
 end
+
+function get_list_runs(list)
+  local run_length = 0
+  local runs = {}
+  for cell in all(list) do
+    if cell == 1 then
+      run_length += 1
+    else
+      if run_length > 0 then
+        add(runs, run_length)
+        run_length = 0
+      end
+    end
+  end
+
+  if run_length > 0 or #runs == 0 then
+    add(runs, run_length)
+  end
+
+  return runs
+end
